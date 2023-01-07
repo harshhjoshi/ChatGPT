da6cfa694c1545d8b14fe68c11591b70ab9da36e
Harsh Joshi <harsh.joshi@archesoftronix.com>
2023-01-08 03:11:48 +0530

    Changes

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
   // messageDiv.innerHTML = "..."
   loader(messageDiv)
 
-  const response = await fetch('http://localhost:3400/', {
+  const response = await fetch('https://chatgpt-gq14.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
